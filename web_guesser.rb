31906fedf0d259993fd369d6020df8fca392b398
S-ANANDS <anandnair96@gmail.com>
2019-05-31 15:23:47 +0530

    Added condition to check whether the entered number is les than or greater than the random generated value and added color

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,33 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  erb :index :locals =>{:number => number}
-  # a = rand(1..100)
+  player_guess = params['guess'].to_i
+  response = check_guess(player_guess)
+
+  erb :index, locals: { answer: RANDOM_NUMBER, player_guess: player_guess, response: response }
+end
+
+RANDOM_NUMBER = rand(1..100)
+
+def check_guess(player_guess)
+  if player_guess == ''.to_i
+    ''
+  elsif player_guess == RANDOM_NUMBER
+    "Response: The secret number was #{RANDOM_NUMBER}"
+  elsif player_guess < RANDOM_NUMBER
+    if player_guess < RANDOM_NUMBER - 5
+      'Response: Much too low'
+    else
+      'Response: Too low'
+    end
+  elsif player_guess > RANDOM_NUMBER
+    if player_guess > RANDOM_NUMBER + 5
+      'Response: Much too high'
+    else
+      'Response: Too high'
+    end
+  end
 end
