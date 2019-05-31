35acc16326324ec226a860cc10d37493d9e2a57a
S-ANANDS <anandnair96@gmail.com>
2019-05-31 16:23:47 +0530

    Change color of the background as input changes

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,24 +7,23 @@ get '/' do
   player_guess = params['guess'].to_i
   response = check_guess(player_guess)
 
-  erb :index, locals: { answer: RANDOM_NUMBER, player_guess: player_guess, response: response }
+  erb :index, locals: { answer: SECRET_NUMBER, player_guess: player_guess, response: response}
 end
-
-RANDOM_NUMBER = rand(1..100)
+SECRET_NUMBER = rand(1..100)
 
 def check_guess(player_guess)
   if player_guess == ''.to_i
     ''
-  elsif player_guess == RANDOM_NUMBER
-    "Response: The secret number was #{RANDOM_NUMBER}"
-  elsif player_guess < RANDOM_NUMBER
-    if player_guess < RANDOM_NUMBER - 5
+  elsif player_guess == SECRET_NUMBER
+    "Response: The secret number was #{SECRET_NUMBER}"
+  elsif player_guess < SECRET_NUMBER
+    if player_guess < SECRET_NUMBER - 5
       'Response: Much too low'
     else
       'Response: Too low'
     end
-  elsif player_guess > RANDOM_NUMBER
-    if player_guess > RANDOM_NUMBER + 5
+  elsif player_guess > SECRET_NUMBER
+    if player_guess > SECRET_NUMBER + 5
       'Response: Much too high'
     else
       'Response: Too high'
